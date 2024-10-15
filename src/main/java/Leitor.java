4dd2ccd163df47e8b5c5b2ebeae405dcad092fa6
jheniferlorrane <jhenifer.rodrigues@sptech.school>
2024-10-14 23:34:31 -0300

    Lendo não apenas coluna, mais linhas também, formatando arquivo deixando os strings em int

--- src/main/java/Leitor.java
+++ src/main/java/Leitor.java
@@ -1,13 +1,15 @@
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.io.InputStream;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Leitor {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
     private final JdbcTemplate jdbcTemplate;
     private final OperacoesBucket operacoesBucket;
 
@@ -17,31 +19,64 @@ public class Leitor {
         this.operacoesBucket = operacoesBucket;
     }
 
-    public void lerArquivo(String bucketName, String arquivoKey) {
+    public boolean lerArquivo(String bucketName, String arquivoKey) {
         try (InputStream inputStream = operacoesBucket.baixarObjeto(bucketName, arquivoKey)) {
             processFile(inputStream);
+            return true; // Retorna true se a leitura foi bem-sucedida
         } catch (Exception e) {
             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+            return false; // Retorna false se houver erro
         }
     }
 
     private void processFile(InputStream inputStream) {
         try (Workbook workbook = new XSSFWorkbook(inputStream)) {
-            // Acessando a primeira planilha
             Sheet sheet = workbook.getSheetAt(0);
+            for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                Row row = sheet.getRow(rowIndex);
+                if (row != null) {
+                    Cell cellEspecificacao = row.getCell(0);
+                    if (cellEspecificacao != null) {
+                        String especificacao = cellEspecificacao.getStringCellValue();
+                        int[] quantidades = new int[12];
+                        for (int i = 1; i <= 12; i++) {
+                            Cell cellQuantidade = row.getCell(i);
+                            if (cellQuantidade != null) {
+                                switch (cellQuantidade.getCellType()) {
+                                    case NUMERIC:
+                                        quantidades[i - 1] = (int) cellQuantidade.getNumericCellValue();
+                                        break;
+                                    case STRING:
+                                        String cellValue = cellQuantidade.getStringCellValue();
+                                        if ("...".equals(cellValue)) {
+                                            quantidades[i - 1] = 0;
+                                        } else {
+                                            try {
+                                                NumberFormat format = NumberFormat.getInstance();
+                                                Number number = format.parse(cellValue.replace(".", "").replace(",", "."));
+                                                quantidades[i - 1] = number.intValue();
+                                            } catch (ParseException e) {
+                                                System.err.println("Erro ao converter a célula para inteiro: " + e.getMessage());
+                                                quantidades[i - 1] = 0;
+                                            }
+                                        }
+                                        break;
+                                    default:
+                                        quantidades[i - 1] = 0;
+                                }
+                            } else {
+                                quantidades[i - 1] = 0;
+                            }
+                        }
 
-            // Iterando sobre as linhas da planilha
-            for (Row row : sheet) {
-                // Assumindo que a primeira célula tem o valor que queremos inserir no BD
-                Cell cell = row.getCell(0);
-                if (cell != null) { // Verifica se a célula não é nula
-                    String valor = cell.getStringCellValue();
-                    System.out.println("Valor lido: " + valor);
-
-                    // Inserindo o valor no banco de dados
-                    jdbcTemplate.update("INSERT INTO crimes (especificacao) VALUES (?)", valor);
-                } else {
-                    System.out.println("Linha " + row.getRowNum() + " está vazia ou a célula é nula.");
+                        for (int i = 0; i < 12; i++) {
+                            jdbcTemplate.update("INSERT INTO crimes (especificacao, qtd_casos, ano, mes) VALUES (?, ?, ?, ?)",
+                                    especificacao, quantidades[i], 2024, i + 1);
+                            System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Foi inserido no banco: " + especificacao + ", Mês: " + (i + 1) + ", Casos: " + quantidades[i]);
+                        }
+                    } else {
+                        System.out.println("Linha " + row.getRowNum() + " está vazia ou a célula de especificação é nula.");
+                    }
                 }
             }
         } catch (Exception e) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 
-import javax.sql.DataSource;  // Certifique-se de que você importou DataSource
-import java.sql.Connection;  // Importar Connection
+import java.sql.Connection;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -17,34 +16,29 @@ public class Main {
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-        // Acessando a variável de ambiente
         String bucketName = System.getenv("S3_BUCKET_NAME");
         String arquivoKey = "OcorrenciaMensal(Criminal)-EstadoSP_20241007_134342.xlsx";
 
         try {
-            // Tenta obter a conexão
             jdbcTemplate = dbConnectionProvider.getConnection();
             Connection connection = null;
 
             try {
-                // Se a conexão for bem-sucedida, loga a mensagem
                 connection = DataSourceUtils.getConnection(jdbcTemplate.getDataSource());
                 System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Conexão com o banco de dados estabelecida com sucesso.");
-
-                // Log de quando o processo começa
                 System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Iniciando o processo de leitura do arquivo do S3.");
 
-                // Ler o arquivo do S3
-                leitor.lerArquivo(bucketName, arquivoKey);
+                boolean leituraBemSucedida = leitor.lerArquivo(bucketName, arquivoKey);
 
-                // Log de análise do arquivo
-                System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Análise do arquivo '" + arquivoKey + "' concluída.");
+                if (leituraBemSucedida) {
+                    System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Análise do arquivo '" + arquivoKey + "' concluída.");
+                } else {
+                    System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Não foi possível ler o arquivo '" + arquivoKey + "'.");
+                }
 
             } catch (DataAccessException e) {
-                // Log de erro de conexão
                 System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Falha ao estabelecer a conexão com o banco de dados: " + e.getMessage());
             } finally {
-                // Fechar a conexão se ela foi estabelecida
                 if (connection != null) {
                     try {
                         DataSourceUtils.releaseConnection(connection, jdbcTemplate.getDataSource());
@@ -55,12 +49,10 @@ public class Main {
             }
 
         } catch (Exception e) {
-            // Log de erro genérico
             System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Erro: " + e.getMessage());
             e.printStackTrace();
         }
 
-        // Log de quando o processo termina
         System.out.println("[" + LocalDateTime.now().format(formatter) + "] - Processo finalizado.");
     }
 }
